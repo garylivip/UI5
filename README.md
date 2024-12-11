3d0277282d33dfa13864d5290a735682f841bd05
Gary Li <gary.li@sap.com>
2024-12-11 16:20:53 +0800

    Step 6: Create and Edit  completed

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ UI5 Practice
 - if npm mirror fails -> try npmjs.org through proxy
 - do not forget delete the proxy, otherwise fiori tools get into trouble
 - tsconfig.json got generated, which impacted the js files, renamed it tsconfig.json.md temporily 
+
+# Batch control
+    - Batch control https://sapui5.hana.ondemand.com/#/topic/74142a38e3d4467c8d6a70b28764048f
+    - Just to simplify the testing, this sample using "groupId": "$direct"` 
+
--- basic_v4/webapp/controller/Home.controller.js
+++ basic_v4/webapp/controller/Home.controller.js
@@ -1,4 +1,5 @@
-sap.ui.define([
+sap.ui.define(
+  [
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast",
@@ -6,55 +7,192 @@ sap.ui.define([
     "sap/ui/model/Filter",
     "sap/ui/model/Sorter",
     "sap/ui/model/FilterOperator",
-    "sap/ui/model/FilterType"
-], (Controller, JSONModel, MessageToast, MessageBox, Filter, Sorter, FilterOperator, FilterType) => {
+    "sap/ui/model/FilterType",
+  ],
+  (
+    Controller,
+    JSONModel,
+    MessageToast,
+    MessageBox,
+    Filter,
+    Sorter,
+    FilterOperator,
+    FilterType
+  ) => {
     "use strict";
 
     return Controller.extend("ns.basicv4.controller.Home", {
-        onInit() {
-            let oJSONData = {busy: false, order: 0 };
-            let oModel = new JSONModel(oJSONData);
-            this.getView().setModel(oModel, "homeView");
-        },
+      onInit() {
+        let oMessageManager = sap.ui.getCore().getMessageManager();
+        let oMessageModel = oMessageManager.getMessageModel();
+        let oMessageModelBinding = oMessageModel.bindList(
+          "/",
+          undefined,
+          [],
+          new Filter("technical", FilterOperator.EQ, true)
+        );
+        let oViewModel = new JSONModel({
+          busy: false,
+          hasUIChanges: false,
+          usernameEmpty: true,
+          order: 0,
+        });
+        this.getView().setModel(oViewModel, "homeView");
+        this.getView().setModel(oMessageModel, "message");
 
-        onRefresh() {
-            var oBinding = this.byId("peopleList").getBinding("items");
-            if (oBinding.hasPendingChanges()) {
-                MessageBox.error(this._getText("refreshNotPossibleMessage"));
-                return;
-            }
-            oBinding.refresh();
-            MessageToast.show(this._getText("refreshSuccessMessage"))
-        },
+        oMessageModelBinding.attachChange(this.onMessageBindingChange, this);
+        this._bTechnicalErrors = false;
+      },
 
-        onSort() {
-            let oView = this.getView();
-            let aStates = [undefined, "asc", "desc"];
-            let aSteteTextIds = ["sortNone", "sortAscending", "sortDescending"];
-            let sMessage;
-            let iOrder = oView.getModel("homeView").getProperty("/order");
-
-            iOrder = (iOrder + 1) % aStates.length;
-            let sOrder = aStates[iOrder];
-            oView.getModel("homeView").setProperty("/order", iOrder);
-
-            let oBinding = oView.byId("peopleList").getBinding("items");
-            oBinding.sort(sOrder && new Sorter("LastName", sOrder === "desc"));
-
-            sMessage = this._getText("sortMessage", [this._getText(aSteteTextIds[iOrder])]);
-            MessageToast.show(sMessage);
-        },
-
-        onSearch() {
-            let oView = this.getView();
-            let sValue = oView.byId("searchField").getValue();
-            let oFilter = new Filter("LastName", FilterOperator.Contains, sValue);
-            let oBinding = oView.byId("peopleList").getBinding("items");
-            oBinding.filter(oFilter, FilterType.Application);
-        },
-
-        _getText(sKey, aArgs) {
-            return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sKey, aArgs);
+      onRefresh() {
+        var oBinding = this.byId("peopleList").getBinding("items");
+        if (oBinding.hasPendingChanges()) {
+          MessageBox.error(this._getText("refreshNotPossibleMessage"));
+          return;
         }
+        oBinding.refresh();
+        MessageToast.show(this._getText("refreshSuccessMessage"));
+      },
+
+      onSort() {
+        let oView = this.getView();
+        let aStates = [undefined, "asc", "desc"];
+        let aSteteTextIds = ["sortNone", "sortAscending", "sortDescending"];
+        let sMessage;
+        let iOrder = oView.getModel("homeView").getProperty("/order");
+
+        iOrder = (iOrder + 1) % aStates.length;
+        let sOrder = aStates[iOrder];
+        oView.getModel("homeView").setProperty("/order", iOrder);
+
+        let oBinding = oView.byId("peopleList").getBinding("items");
+        oBinding.sort(sOrder && new Sorter("LastName", sOrder === "desc"));
+
+        sMessage = this._getText("sortMessage", [
+          this._getText(aSteteTextIds[iOrder]),
+        ]);
+        MessageToast.show(sMessage);
+      },
+
+      onSearch() {
+        let oView = this.getView();
+        let sValue = oView.byId("searchField").getValue();
+        let oFilter = new Filter("LastName", FilterOperator.Contains, sValue);
+        let oBinding = oView.byId("peopleList").getBinding("items");
+        oBinding.filter(oFilter, FilterType.Application);
+      },
+
+      onMessageBindingChange(oEvent) {
+        let aContexts = oEvent.getSource().getContexts();
+        let aMessages;
+        let bMessageOpen = false;
+
+        if (bMessageOpen || aContexts.length === 0) {
+          return;
+        }
+
+        aMessages = aContexts.map((oContext) => {
+          return oContext.getObject();
+        });
+        sap.ui.getCore().getMessageManager().removeMessages(aMessages);
+
+        this._setUIChanges(true);
+        this._bTechnicalErrors = false;
+        MessageBox.error(aMessages[0].message, {
+          id: "serviceErrorMessageBox",
+          details: aMessages[0].description,
+          actions: [MessageBox.Action.CLOSE],
+          onClose: () => {
+            bMessageOpen = false;
+          },
+        });
+      },
+
+      onCreate() {
+        let oList = this.byId("peopleList");
+        let oBinding = oList.getBinding("items");
+        let oContext = oBinding.create({
+          UserName: "",
+          FirstName: "",
+          LastName: "",
+          Age: "18",
+        });
+
+        this._setUIChanges();
+        this.getView().getModel("homeView").setProperty("/usernameEmpty", true);
+
+        oList.getItems().some((oItem) => {
+          if (oItem.getBindingContext() === oContext) {
+            oItem.focus();
+            oItem.setSelected(true);
+            return true;
+          }
+        });
+      },
+
+      onSave() {
+        console.log("save xxxxxxxxxxxxxxxxxxxxxx");
+        
+        var fnSuccess = function () {
+          this._setBusy(false);
+          MessageToast.show(this._getText("changesSentMessage"));
+          this._setUIChanges(false);
+        }.bind(this);
+
+        var fnError = function () {
+          this._setBusy(false);
+          this._setUIChanges(false);
+          MessageBox.error(oError.message);
+        }.bind(this);
+
+        this._setBusy(true); // Lock UI until submitBatch is resolved.
+        this.getView()
+          .getModel()
+          .submitBatch("peopleGroup")
+          .then(fnSuccess, fnError);
+        this._bTechnicalErrors = false;
+      },
+
+      onResetChanges(){
+        console.log("reset xxxxxxxxxxxxxxxxxxxxxx");
+        
+        this.byId("peopleList").getBinding("items").resetChanges();
+        this._bTechnicalErrors = false;
+        this._setUIChanges();  
+      },
+
+      onInputChange(oEvent) {
+        if (oEvent.getParameter("escPressed") ) {
+          this._setUIChanges();
+        } else {
+          if (oEvent.getSource().getParent().getBindingContext().hasPendingChanges()) {
+            this.getView().getModel("homeView").setProperty("/usernameEmpty", false);
+          }
+        }
+      },
+
+      _setUIChanges(bHasUIChanges) {
+        if (this._bTechnicalErrors) {
+          bHasUIChanges = true;
+        } else if (bHasUIChanges === undefined) {
+          bHasUIChanges = this.getView().getModel().hasPendingChanges();
+        }
+
+        let oModel = this.getView().getModel("homeView");
+        oModel.setProperty("/hasUIChanges", bHasUIChanges);
+      },
+
+      _getText(sKey, aArgs) {
+        return this.getOwnerComponent()
+          .getModel("i18n")
+          .getResourceBundle()
+          .getText(sKey, aArgs);
+      },
+
+      _setBusy(bIsBusy) {
+        let oModel = this.getView().getModel("homeView");
+        oModel.setProperty("/busy", bIsBusy);
+      }
     });
-});
+  }
+);
